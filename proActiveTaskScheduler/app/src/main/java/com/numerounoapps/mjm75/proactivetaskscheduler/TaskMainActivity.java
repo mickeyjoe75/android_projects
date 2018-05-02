49cdbbae5afc1812c2a29c5fc3e4499a0e232ef8
Joseph Manuel <mickeyjoe75@gmail.com>
2018-05-02 11:47:51 +0100

    setup files

---
+++ proActiveTaskScheduler/app/src/main/java/com/numerounoapps/mjm75/proactivetaskscheduler/TaskMainActivity.java
@@ -0,0 +1,59 @@
+package com.numerounoapps.mjm75.proactivetaskscheduler;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class TaskMainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private TextView date;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+        //create a date string.
+        String date_n = new SimpleDateFormat("MMM dd, yyyy", Locale.getDefault()).format(new Date());
+        //get hold of textview.
+        date = findViewById(R.id.dateTextId);
+        //set it as current date.
+        date.setText(date_n);
+
+
+    }
+
+}
