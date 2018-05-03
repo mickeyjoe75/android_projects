91b47642489584933588c0f87ffe88eb05d71297
Joseph Manuel <mickeyjoe75@gmail.com>
2018-05-03 20:59:24 +0100

    final commit after mvp done,for the ppt

--- proActiveTaskScheduler/app/src/main/java/com/numerounoapps/mjm75/proactivetaskscheduler/HomeFragment.java
+++ proActiveTaskScheduler/app/src/main/java/com/numerounoapps/mjm75/proactivetaskscheduler/HomeFragment.java
@@ -1,14 +1,11 @@
 package com.numerounoapps.mjm75.proactivetaskscheduler;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-
-
+        import android.os.Bundle;
+        import android.support.annotation.Nullable;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
 
 
 public class HomeFragment extends Fragment {
--- proActiveTaskScheduler/app/src/main/java/com/numerounoapps/mjm75/proactivetaskscheduler/SqlDbHelper.java
+++ proActiveTaskScheduler/app/src/main/java/com/numerounoapps/mjm75/proactivetaskscheduler/SqlDbHelper.java
@@ -10,13 +10,12 @@ import android.database.sqlite.SQLiteDatabase;
 import java.util.ArrayList;
 
 
-
 public class SqlDbHelper extends SQLiteOpenHelper {
 
-    private static final String DB_NAME="proactivets";
+    private static final String DB_NAME = "proactivets";
     private static final int DB_VER = 1;
-    public static final String DB_TABLE="Task";
-    public static final String DB_COLUMN = "TaskName";
+    public static final String DB_TABLE = "Task";
+    public static final String DB_COLUMN_1 = "TaskName";
 
     public SqlDbHelper(Context context) {
         super(context, DB_NAME, null, DB_VER);
@@ -24,39 +23,37 @@ public class SqlDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String query = String.format("CREATE TABLE %s (ID INTEGER PRIMARY KEY AUTOINCREMENT,%s TEXT NOT NULL);",DB_TABLE,DB_COLUMN);
+        String query = String.format("CREATE TABLE %s (ID INTEGER PRIMARY KEY AUTOINCREMENT,%s TEXT NOT NULL);", DB_TABLE, DB_COLUMN_1);
         db.execSQL(query);
-
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        String query = String.format("DELETE TABLE IF EXISTS %s",DB_TABLE);
+        String query = String.format("DELETE TABLE IF EXISTS %s", DB_TABLE);
         db.execSQL(query);
         onCreate(db);
-
     }
 
-    public void insertNewTask(String task){
-        SQLiteDatabase db= this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(DB_COLUMN,task);
-        db.insertWithOnConflict(DB_TABLE,null,values,SQLiteDatabase.CONFLICT_REPLACE);
-        db.close();
-    }
-
-    public void deleteTask(String task){
+    public void insertNewTask(String task) {
         SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(DB_TABLE,DB_COLUMN + " = ?",new String[]{task});
+        ContentValues values = new ContentValues();
+        values.put(DB_COLUMN_1, task);
+        db.insertWithOnConflict(DB_TABLE, null, values, SQLiteDatabase.CONFLICT_REPLACE);
         db.close();
     }
 
-    public ArrayList<String> getTaskList(){
+    public void deleteTask(String task) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(DB_TABLE, DB_COLUMN_1 + " = ?", new String[]{task});
+        db.close();
+    }
+
+    public ArrayList<String> getTaskList() {
         ArrayList<String> taskList = new ArrayList<>();
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.query(DB_TABLE,new String[]{DB_COLUMN},null,null,null,null,null);
-        while(cursor.moveToNext()){
-            int index = cursor.getColumnIndex(DB_COLUMN);
+        Cursor cursor = db.query(DB_TABLE, new String[]{DB_COLUMN_1}, null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            int index = cursor.getColumnIndex(DB_COLUMN_1);
             taskList.add(cursor.getString(index));
         }
         cursor.close();
